9c173c59e53784b9624dc5db5fe2191ad1325b73
kallDun <igorxlonline24@gmail.com>
2021-11-23 13:07:26 +0200

    first commit

---
+++ Lab_5/EffectiveCode.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab_5
+{
+    class EffectiveCode
+    {
+        int countOfSymbols;
+        Dictionary<string, double> dictionary;
+        Dictionary<string, string> combination = new();
+        public EffectiveCode(Dictionary<string, double> dictionary, int countOfSymbols)
+        {
+            this.dictionary = dictionary;
+            this.countOfSymbols = countOfSymbols;
+            foreach (var item in dictionary)
+            {
+                combination.Add(item.Key, "");
+            }
+        }
+
+        public Dictionary<string, string> CreateCode()
+        {
+            var sorted = dictionary.OrderBy(x => -x.Value).Select(x => x.Key).ToList();
+            SplitArrayAndTypeCode(sorted);
+            return combination;
+        }
+
+        private void SplitArrayAndTypeCode(List<string> list)
+        {
+            if (list.Count <= 1) return;
+
+            int divide_in = 1;
+            double delta = -1;
+            for (int i = 1; i < list.Count; i++)
+            {
+                double sum_first_part = 0, sum_second_part = 0;
+                for (int j = 0; j < i; j++) sum_first_part += dictionary[list[j]];
+                for (int j = i; j < list.Count; j++) sum_second_part += dictionary[list[j]];
+
+                var new_delta = Math.Abs(sum_first_part - sum_second_part);
+                if (new_delta == 0 || i == list.Count - 1)
+                {
+                    divide_in = i;
+                    break;
+                }
+                if (delta != -1 && new_delta > delta)
+                {
+                    divide_in = i - 1;
+                    break;
+                }
+                delta = new_delta;
+            }
+
+            var list_1 = list.Take(divide_in).ToList();
+            var list_2 = list.Skip(divide_in).ToList();
+
+            list_1.ForEach(x => combination[x] += "1");
+            list_2.ForEach(x => combination[x] += "0");
+
+            SplitArrayAndTypeCode(list_1);
+            SplitArrayAndTypeCode(list_2);
+        }
+
+        public double GetEntropia()
+            => dictionary.Select(x => -1 * x.Value * Math.Log(x.Value, 2)).Sum();
+        public double GetAverageNumberLength()
+            => dictionary.Select(x => x.Value * combination[x.Key].Length).Sum() / countOfSymbols;
+    }
+}
---
+++ Lab_5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+            Console.WriteLine("Type count of symbols in one letter. " +
+                "For 'AA' type 2, for 'A' - 1, for 'AAA' - 3");
+            int countOfSymbols = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Type alphabet symbol and chance like 'AA 0.15'\n" +
+                "To exit TypeMenu type 'exit' or 'close'");
+            Dictionary<string, double> dictionary = new();
+
+            string key = "";
+            double value;
+            do
+            {
+                (key, value) = GetPairForDictionary();
+                if (string.IsNullOrEmpty(key)) continue;
+                if (dictionary.ContainsKey(key))
+                {
+                    Console.WriteLine("Dictionary already has this symbol!");
+                    continue;
+                }
+                if (value >= 1 || value <= 0)
+                {
+                    Console.WriteLine("Chance must be between 0 and 1");
+                    continue;
+                }
+                dictionary.Add(key, value);
+            }
+            while (!string.IsNullOrEmpty(key));
+            if (dictionary.Sum(x => x.Value) != 1) throw new Exception("Sum of chances must be = 1");
+
+            EffectiveCode effectiveCode = new(dictionary, countOfSymbols);
+            var code = effectiveCode.CreateCode();
+
+            var output = string.Join("\n", code.Select(x => $"symbol: {x.Key} with code: {x.Value}"));
+            Console.WriteLine(output);
+
+            var entropia = effectiveCode.GetEntropia();
+            var average = effectiveCode.GetAverageNumberLength();
+            var effectively_percent = Math.Abs(average - entropia) / average * 100;
+            Console.WriteLine($"Entropia: {entropia}, Average: {average}, Effective: {effectively_percent}%");
+
+            Console.ReadKey();
+        }
+
+        static (string, double) GetPairForDictionary()
+        {
+            var text = Console.ReadLine();
+            if (string.IsNullOrEmpty(text) || text == "close" || text == "exit") return ("", 0);
+            
+            var typed = text.Split();
+            return (typed[0], double.Parse(typed[1]));
+        }
+    }
+}
