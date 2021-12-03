1c21cc3342f6aaaf02fb6358f3544597b2e5ae29
kallDun <igorxlonline24@gmail.com>
2021-12-03 23:58:28 +0200

    commit

--- Lab_1/Program.cs
+++ Lab_1/Program.cs
@@ -1,10 +1,6 @@
 ﻿using Lab_1.Classes;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Lab_1
 {
