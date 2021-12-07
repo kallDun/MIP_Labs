2d0e312191b713f9233d4ba6ecfe1ce067c1ee5d
kallDun <igorxlonline24@gmail.com>
2021-12-07 12:08:56 +0200

    lab 4 done

---
+++ Lab_4/Task_4.3.3.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab_4
+{
+    class Task__4_3
+    {
+        private string code_A;
+        private int n;
+        private int d;
+
+        public Task__4_3(string a, int n, int d)
+        {
+            code_A = a;
+            this.n = n;
+            this.d = d;
+        }
+
+        public string[] GetCombinations() => CodeHelper.GenerateAllCombinationsByDistance(code_A, d);
+    }
+}
